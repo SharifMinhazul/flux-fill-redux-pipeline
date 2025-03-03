21a1b7710192adc598fedfe72c5f84d51461a7b7
Emon <emon@kitegamesstudio.com>
2025-03-03 15:35:36 +0600

    feat: add initial implementations for various encoders and tokenizers, including Redux processing

--- utils/redux_processor.py
+++ utils/redux_processor.py
@@ -1,10 +1,23 @@
+import json
+import logging
+import os
 from PIL import Image
+import safetensors
+import torch
+
+from utils.misc.clip_vision import load
 
 class ReduxProcessor:
     """Handles Redux model processing."""
+
+    def load_clip(self, clip_path: str) -> tuple:
+        """Load CLIP model."""
+        clip_vision = load(clip_path)
+        return (clip_vision,)
     
     @staticmethod
     def apply_redux(image: Image) -> Image:
         """Process outfit image using Redux."""
         # TODO: Implement Redux model inference
         return image
+    
